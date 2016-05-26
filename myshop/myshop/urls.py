209790c91faed365ab3b98f60b1bb55949cb5ea6
Validate Health <andrew.webster@validatehealth.com>
2016-05-25 21:15:43 -0500

    Fix URLs issue

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import include, url, static
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'extra_context': {'next':'/'}}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/logged_out/'}, name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+    url(r'^login/$', views.login, {'extra_context': {'next':'/'}}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/logged_out/'}, name='logout'),
+    url(r'^logout-then-login/$', views.logout_then_login, name='logout_then_login'),
     url(r'^', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
