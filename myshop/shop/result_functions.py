4ed69890b201690f91e36cb2bf3cd97f80f358b3
njpatnode <nick.patnode@validatehealth.com>
2016-06-14 15:14:42 -0500

    Added Lasso, Ridge, and Linear Regressions
    x_variables is undefined.. this should be defined for our selection of multiple variables

--- myshop/shop/result_functions.py
+++ myshop/shop/result_functions.py
@@ -24,7 +24,7 @@ class result_function(object):
             fig, ax = plt.subplots()
             x = np.array(dataview_df[x_variable])
             y = np.array(dataview_df[y_variable])
-            ax.scatter(x, y, s=int(dot_size))
+            ax.scatter(x, y, s=dot_size)
             ax.set_xlabel(x_variable)
             ax.set_ylabel(y_variable)
             ax.set_title(x_variable + 'vs.' + y_variable + 'Scatterplot')
@@ -34,4 +34,71 @@ class result_function(object):
             fig_html = mpld3.fig_to_html(fig)
         else:
             fig_html = "Error: No y variable was provided."
+        return fig_html
+    def lasso(self, dataview_df, x_variables, y_variable=None, test_split=0.2, alpha_value=1):
+        if y_variable is not None:
+            from sklearn.linear_model import Lasso
+            import seaborn as sns
+            fig, ax = plt.subplots()
+            lrg = Lasso(alpha = alpha_value)
+            l_train, l_test, j_train, j_test = train_test_split(x_variables, dataview.df[y_variable], test_size = test_split)
+            lrg.fit(l_train,j_train)
+            lrg_pred = lrg.predict(l_test)
+            ax = sns.regplot(j_test, lrg_pred, ci = None)
+            lassoscore = str(lrg.score(l_train, j_train))
+            ax.set_xlabel('Predicted Test Data')
+            ax.set_ylabel('Input Test Data')
+            ax.set_xlim(-0.05,)
+            ax.set_ylim(-0.05,)
+            ax.set_title('Lasso Regression \n Lasso Score equals: ' + lassoscore)
+            fig_html = mpld3.fig_to_html(fig)
+        else:
+            fig_html = "Error: No y variable was provided."
+        return fig_html
+    def ridge(self, dataview.df, x_variables, y_variable=None, test_split=0.2, alpha_value=1):
+        if y_variable is not None:   
+            from sklearn.linear_model import Ridge
+            import seaborn as sns
+            fig, ax = plt.subplots()
+            rdg = Ridge(alpha = alpha_value)
+            r_train, r_test, s_train, s_test = train_test_split(x_variables, dataview.df[y_variable], test_size = test_split)
+            rdg.fit(r_train, s_train)
+            rdgscore = str(rdg.score(r_train, s_train))
+            rdg_pred = rdg.predict(r_test)
+            ax = sns.regplot(s_test, rdg_pred, ci = None)
+            ax.set_xlabel('Predicted Test Data')
+            ax.set_ylabel('Input Test Data')
+            ax.set_xlim(-0.05,)
+            ax.set_ylim(-0.05,)
+            ax.set_title('Ridge Regression \n Ridge Score equals: ' + rdgscore)
+            fig_html = mpld3.fig_to_html(fig)
+        else:
+            fig_html = "Error: No y variable was provided."   
+        return fig_html
+    def lin_reg(self, dataview.df, x_variables, y_variable=None, test_split=0.2):
+        if y_variable is not None:
+            import scipy
+            from scipy import stats
+            import pandas as pd
+            import matplotlib.pyplot as plt
+            import numpy as np
+            from sklearn.linear_model import LinearRegression
+            from sklearn.cross_validation import train_test_split
+            from sklearn.metrics import r2_score
+            from sklearn import metrics
+            fig, ax = plt.subplots()
+            x_train, x_test, y_train, y_test = train_test_split(x_variables, dataview.df[y_variable], test_size=test_split)
+            regr = LinearRegression()
+            regr.fit(x_train,y_train)
+            y_pred = regr.predict(x_test)
+            ax = sns.regplot(y_test, y_pred, ci=None)
+            ax.set_xlabel('Predicted Test Data')
+            ax.set_ylabel('Input Test Data')
+            ax.set_xlim(-0.05,)
+            ax.set_ylim(-0.05,)
+            rsquared = str(r2_score(y_test,y_pred))
+            ax.set_title('Linear Regression \n R^2 score: ' + rsquared)
+            fig_html = mpld3.fig_to_html(fig)
+        else:
+            fig_html = "Error: No y variable was provided."           
         return fig_html
